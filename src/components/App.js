3efaae0d24ca5fbc06023e0fb60230a462244ce5
Timothy Ryon <tim@timryon.com>
2024-02-04 14:47:22 -0500

    finished lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,33 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+const[dogData, setDogData] = useState({
+    message:"",
+    status:"",
+});
+const[isLoaded, setIsLoaded] = useState(false);
+
+let { message, status } = dogData;
+const baseUrl = "https://dog.ceo/api/breeds/image/random"
+
+    useEffect(()=>{
+        fetch(baseUrl)
+        .then(response => response.json())
+        .then((response) => {
+            setDogData(() => message = response.message )
+            setIsLoaded(!isLoaded); 
+        })
+    }, [])
+
+    const loadingMsg = () => !isLoaded ? <p>Loading...</p> : 
+    <img src = { dogData } alt ="A Random Dog"></img>;
+   
+    return (
+        <>
+            {/* <div> { !isLoaded ? <p>"Loading..."</p> : null }</div> */}
+            <div> { loadingMsg() }</div>
+        </>    
+    )
+}
+
+export default App;
